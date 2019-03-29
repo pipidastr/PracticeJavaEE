4a5973d5f9ed26f488a7a69f893eaa1f2a158362
Dmitriy Grachev <grachevdmitriy152@gmail.com>
2019-03-29 16:51:16 +0300

    change script

--- Dump20190323.sql
+++ Dump20190323.sql
@@ -1,30 +1,5 @@
--- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: sys
--- ------------------------------------------------------
--- Server version	8.0.15
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-03-23 21:06:19
+CREATE DATABASE  IF NOT EXISTS `storage` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `storage`;
 -- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: storage
@@ -50,9 +25,10 @@ DROP TABLE IF EXISTS `consumers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `consumers` (
-  `consumer_ID` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`consumer_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `consumer_name` varchar(45) DEFAULT 'Not name',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -61,6 +37,7 @@ CREATE TABLE `consumers` (
 
 LOCK TABLES `consumers` WRITE;
 /*!40000 ALTER TABLE `consumers` DISABLE KEYS */;
+INSERT INTO `consumers` VALUES (1,NULL),(2,NULL),(3,NULL),(4,NULL),(5,NULL),(6,NULL),(7,NULL),(8,NULL),(9,NULL),(10,NULL),(11,NULL),(12,NULL),(21,NULL),(22,NULL);
 /*!40000 ALTER TABLE `consumers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -72,12 +49,12 @@ DROP TABLE IF EXISTS `order`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `order` (
-  `order_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `consumer_ID` int(11) NOT NULL,
-  PRIMARY KEY (`order_ID`),
+  PRIMARY KEY (`ID`),
   KEY `consumer_ID_idx` (`consumer_ID`),
-  CONSTRAINT `consumer_ID` FOREIGN KEY (`consumer_ID`) REFERENCES `consumers` (`consumer_ID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `consumer_ID` FOREIGN KEY (`consumer_ID`) REFERENCES `consumers` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -86,6 +63,7 @@ CREATE TABLE `order` (
 
 LOCK TABLES `order` WRITE;
 /*!40000 ALTER TABLE `order` DISABLE KEYS */;
+INSERT INTO `order` VALUES (10,1),(12,1),(1,2),(5,2),(7,2),(2,3),(6,3),(3,6),(9,8),(8,9);
 /*!40000 ALTER TABLE `order` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -97,12 +75,13 @@ DROP TABLE IF EXISTS `order_product`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `order_product` (
-  `order_product_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID` int(11) NOT NULL,
   `product_ID` int(11) NOT NULL,
-  PRIMARY KEY (`order_product_ID`),
+  `order_product_count` int(11) NOT NULL,
   KEY `product_ID_idx` (`product_ID`),
-  CONSTRAINT `order_ID` FOREIGN KEY (`order_product_ID`) REFERENCES `order` (`order_ID`),
-  CONSTRAINT `product_ID` FOREIGN KEY (`product_ID`) REFERENCES `products` (`product_ID`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `order_ID` (`ID`),
+  CONSTRAINT `order_ID` FOREIGN KEY (`ID`) REFERENCES `order` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `product_ID` FOREIGN KEY (`product_ID`) REFERENCES `products` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -112,6 +91,7 @@ CREATE TABLE `order_product` (
 
 LOCK TABLES `order_product` WRITE;
 /*!40000 ALTER TABLE `order_product` DISABLE KEYS */;
+INSERT INTO `order_product` VALUES (2,17,1),(2,18,1);
 /*!40000 ALTER TABLE `order_product` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -123,15 +103,15 @@ DROP TABLE IF EXISTS `products`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `products` (
-  `product_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `product_name` varchar(45) NOT NULL,
   `product_count` int(11) NOT NULL,
   `product_price` double NOT NULL,
   `product_provider_ID` int(11) NOT NULL,
-  PRIMARY KEY (`product_ID`),
+  PRIMARY KEY (`ID`),
   KEY `provider_idx` (`product_provider_ID`),
-  CONSTRAINT `provider` FOREIGN KEY (`product_provider_ID`) REFERENCES `providers` (`provider_ID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `provider` FOREIGN KEY (`product_provider_ID`) REFERENCES `providers` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -140,7 +120,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'zero',200,99.99,1);
+INSERT INTO `products` VALUES (15,'Coca-Cola ZERO',150,89.99,3),(16,'Fanta',23,45,4),(17,'Pepsy',123,54,5),(18,'Mirinda',4,32,6),(19,'Sprite',332,123,5),(20,'Milk',45,435,5),(21,'Water',234,3231,6),(22,'Lime',354,323,7);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -152,10 +132,10 @@ DROP TABLE IF EXISTS `providers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `providers` (
-  `provider_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `provider_name` varchar(45) NOT NULL,
-  PRIMARY KEY (`provider_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -164,7 +144,7 @@ CREATE TABLE `providers` (
 
 LOCK TABLES `providers` WRITE;
 /*!40000 ALTER TABLE `providers` DISABLE KEYS */;
-INSERT INTO `providers` VALUES (1,'Coca-Cola'),(2,'Apple'),(3,'Mera'),(4,'Intel'),(5,'Epam');
+INSERT INTO `providers` VALUES (3,'Test'),(4,'Intel'),(5,'new'),(6,'AMD'),(7,'Rest'),(8,'Apache'),(9,'Easy'),(10,'Gold');
 /*!40000 ALTER TABLE `providers` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -177,4 +157,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-03-23 21:06:19
+-- Dump completed on 2019-03-29 16:50:39
